2f81612c8cb6867967606b84d6cf5e9dc160b641
Mary Sigmon <marysigmon@Marys-MacBook-Pro-2.local>
2023-12-03 20:52:21 -0500

    update

--- index.js
+++ index.js
@@ -1,96 +1,105 @@
+// User enters the website to find a list of names, dates, times, locations, and descriptions
+
+// Get party elements 
+const partiesContainer = document.getElementById('parties-container');
+const newPartyFormContainer = document.getElementById('new-party-form');
+
+// Variable for our API URL
+const API_URL = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-GHP-ET-WEB-FT-SF/events'
+
 // Function to delete a party
 async function deleteParty(id) {
     try {
-      await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
         method: 'DELETE',
-      });
+        });
 
-      // Remove the party from the list
-      parties = parties.filter(party => party.id !== id);
+        // Remove the party from the list
+        parties = parties.filter(party => party.id !== id);
 
-      // Render the updated list
-      renderParties();
-    } catch (error) {
-      console.error('Error deleting party:', error);
-    }
-  }
+        // Render the updated list
+        renderParties();
+    }   catch (error) {
+        console.error('Error deleting party:', error);
+        }
+}
 
-  // Function to render parties on the webpage
-  function renderParties() {
+// Function to render parties on the webpage
+function renderParties() {
     const partyList = document.getElementById('partyList');
     partyList.innerHTML = ''; // Clear the existing list
 
     parties.forEach(party => {
-      const li = document.createElement('li');
-      li.innerHTML = `
+        const li = document.createElement('li');
+        li.innerHTML = `
         <strong>${party.title}</strong> - ${party.body}
         <button onclick="deleteParty(${party.id})">Delete</button>
-      `;
-      partyList.appendChild(li);
+        `;
+        partyList.appendChild(li);
     });
-  }
+}
 
-  // Function to fetch parties from the server
-  async function fetchParties() {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-      const data = await response.json();
-      parties = data.slice(0, 5); // Limit the number of parties for demo
-      renderParties();
-    } catch (error) {
-      console.error('Error fetching parties:', error);
+// Function to fetch parties from the server
+    const fetchAllParties = async () => {
+        try {
+            const response = await fetch(API_URL);
+            const parties = await response.json();
+            console.log(parties);
+            return parties; 
+        } catch (error) {
+            console.error(error);
+        }
     }
-  }
 
-  // Array to store party data
-  let parties = [];
+// Array to store party data
+let parties = [];
 
-  // Form to add a new party
-  const partyForm = document.createElement('form');
-  partyForm.innerHTML = `
-    <h2>Schedule a New Party</h2>
-    <label for="title">Title:</label>
-    <input type="text" id="title" required>
-    <br>
-    <label for="body">Description:</label>
-    <input type="text" id="body" required>
-    <br>
-    <button type="button" onclick="addParty()">Submit</button>
-  `;
+// Form to add a new party
+    const partyForm = document.createElement('form');
+    partyForm.innerHTML = `
+        <h2>Schedule a New Party</h2>
+        <label for="title">Title:</label>
+        <input type="text" id="title" required>
+        <br>
+        <label for="body">Description:</label>
+        <input type="text" id="body" required>
+        <br>
+        <button type="button" onclick="addParty()">Submit</button>
+        `;
 
-  // Function to add a new party
-  async function addParty() {
+// Function to add a new party
+async function addParty() {
     try {
-      const title = document.getElementById('title').value;
-      const body = document.getElementById('body').value;
+        const title = document.getElementById('title').value;
+        const body = document.getElementById('body').value;
 
-      // Perform a POST request to add a new party
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        // Perform a POST request to add a new party
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ title, body }),
-      });
+        });
 
-      const newParty = await response.json();
+        const newParty = await response.json();
 
-      // Add the new party to the array
-      parties.unshift(newParty);
+        // Add the new party to the array
+        parties.unshift(newParty);
 
-      // Clear the form fields
-      document.getElementById('title').value = '';
-      document.getElementById('body').value = '';
+        // Clear the form fields
+        document.getElementById('title').value = '';
+        document.getElementById('body').value = '';
 
-      // Render the updated list of parties
-      renderParties();
-    } catch (error) {
-      console.error('Error adding party:', error);
-    }
-  }
+        // Render the updated list of parties
+        renderParties();
+    }   catch (error) {
+        console.error('Error adding party:', error);
+        }
+}
 
-  // Append the form to the body
-  document.body.appendChild(partyForm);
+// Append the form to the body
+document.body.appendChild(partyForm);
 
-  // Initial fetching of parties
-  fetchParties();
+// Initial fetching of parties
+fetchAllParties();
